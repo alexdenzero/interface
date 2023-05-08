e23fa1fb715287c01cd709dfae5aaf5c98d2ac05
alexdenzero <alexdenzero@yandex.ru>
2023-05-08 08:29:39 +0300

    oop

---
+++ src/Bicycle.java
@@ -0,0 +1,8 @@
+public class Bicycle extends WheeledTransport {
+    public Bicycle(String modelName) {
+        super(modelName, 2);
+    }
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+}
